2bf9acffb68e42f15f1a1bba2ff0e8dcd5831e78
unknown <nilphym@gmail.com>
2021-03-24 00:01:46 +0100

    Add routing example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Authors() {
+  return <h2>Authors</h2>;
+}
 
 function App() {
   return (
-    <div></div>
+    <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/authors">Authors</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/authors">
+            <Authors />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
