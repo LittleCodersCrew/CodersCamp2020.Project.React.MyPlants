073a7325f7ee8698e06932f967bf63164a535f17
unknown <nilphym@gmail.com>
2021-03-24 00:45:59 +0100

    Merge with dev and fix code

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -16,18 +16,18 @@ function Navbar() {
 //   )
   return (
     <ul className="nav-bar">
-          <li className="nav-li">
-            <a className="navbar-link" href="#plant">Plants</a>
-            <img src= { Search } alt="search" height="40px" width="40px"/>
-          </li>
-          <li className="nav-li">
-            <a className="navbar-link" href="#chat">Chat</a>
-            <img src= { Chat } alt="chat" height="40px" width="40px"/>
-          </li>
-          <li className="nav-li">
-            <a className="navbar-link" href="#login">Login</a>
-            <img src= { Profile } alt="profile" height="40px" width="40px"/>
-          </li>
+      <li className="nav-li">
+        <a className="navbar-link" href="#plant">Plants</a>
+        <img src={Search} alt="search" height="40px" width="40px" />
+      </li>
+      <li className="nav-li">
+        <a className="navbar-link" href="#chat">Chat</a>
+        <img src={Chat} alt="chat" height="40px" width="40px" />
+      </li>
+      <li className="nav-li">
+        <a className="navbar-link" href="#login">Login</a>
+        <img src={Profile} alt="profile" height="40px" width="40px" />
+      </li>
     </ul>
   );
 }
