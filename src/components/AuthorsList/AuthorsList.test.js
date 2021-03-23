b14410fcd3c67787c8a5c1e97f172cde95205fcf
Kamila Grusza <kamila.grusza@tlen.pl>
2021-03-23 15:56:51 +0100

    change tests

--- src/components/AuthorsList/AuthorsList.test.js
+++ src/components/AuthorsList/AuthorsList.test.js
@@ -2,7 +2,9 @@ import { render, screen } from '@testing-library/react';
 import AuthorsList from './AuthorsList';
 
 test('rendering AuthorsList component', () => {
+  const names = ['Adrianna Krupa', 'Dominik Puchała', 'Jędrzej Ratajczak', 'Kamila Grusza', 'Konrad Mierzejewski', 'Weronika Brzęczkowska-Kuzianik', 'Filip Kuca - mentor'];
   render(<AuthorsList />);
-  const linkElement = screen.getByText(/Kamila/i);
-  expect(linkElement).toBeInTheDocument();
+  names.forEach(name => {
+    expect(screen.getByText(name)).toBeInTheDocument();
+  });
 });
