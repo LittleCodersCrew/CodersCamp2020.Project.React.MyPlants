712dfd57d55a47817b46778598944c7e805e1185
Kamila Grusza <kamila.grusza@tlen.pl>
2021-03-23 16:41:19 +0100

    change one link

--- src/components/AuthorsList/AuthorsList.js
+++ src/components/AuthorsList/AuthorsList.js
@@ -6,7 +6,7 @@ function AuthorsList() {
     <div className="authors">
       <a className="link" href="https://github.com/adax10" target="_blank" rel="noreferrer">Adrianna Krupa</a>
       <a className="link" href="https://github.com/Suegro24" target="_blank" rel="noreferrer">Dominik Puchała</a>
-      <a className="link" href="https://github.com/Mrozelek" target="_blank" rel="noreferrer">Jędrzej Ratajczak</a>
+      <a className="link" href="https://github.com/Nilphym" target="_blank" rel="noreferrer">Jędrzej Ratajczak</a>
       <a className="link" href="https://github.com/kami3la" target="_blank" rel="noreferrer">Kamila Grusza</a>
       <a className="link" href="https://github.com/KonradMierzejewski" target="_blank" rel="noreferrer">Konrad Mierzejewski</a>
       <a className="link" href="https://github.com/brzeczkowskaw" target="_blank" rel="noreferrer">Weronika Brzęczkowska-Kuzianik</a>
