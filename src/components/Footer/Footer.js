8f250222f16eb148e479603cefbf2a847e2531ca
Weronika Brzęczkowska <brzeczkowska.w@gmail.com>
2021-03-23 21:17:19 +0100

    Footer component added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import './Footer.scss';
+import React from 'react';
+
+import Heart from '../../assets/icons/Heart.png';
+
+function Footer() {
+  return (
+    <footer className="footer-container">
+        <div className="link-container">
+            <img src= { Heart } alt="heart" height="40px" width="40px"/>
+            <a className="footer-link" href="/authors">Authors</a>
+        </div>
+        <p className="copyright">©2021 Copyright</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
+
+test('rendering Footer component', () => {
+    const names = ['Authors'];
+    render(<Footer />);
+    names.forEach(name => {
+      expect(screen.getByText(name)).toBeInTheDocument();
+    });
+  });
