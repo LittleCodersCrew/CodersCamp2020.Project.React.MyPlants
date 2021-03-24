42ab1137eaf09076ba8f93c73591d53a4f4b464e
Weronika Brzęczkowska <brzeczkowska.w@gmail.com>
2021-03-24 20:06:36 +0100

    logged in/out changes added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,61 +8,26 @@ import Users from '../../assets/icons/3 User.png';
 import Calendar from '../../assets/icons/Calendar.png';
 import Leaf from '../../assets/icons/Leaf.png';
 
-function Navbar() {
-//   let loggedOut = (
-//       <ul className={styles.navbar}>
-//           <li className={styles.li}>
-//             <a className={window.location.pathname === '/plant' ?
-//              `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/plant">Plants</a>
-//             <img src= { Search } alt="search" height="40px" width="40px"/>
-//           </li>
-//           <li className={styles.li}>
-//             <a className={window.location.pathname === '/chat' ?
-//             `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/chat">Chat</a>
-//             <img src= { Chat } alt="chat" height="40px" width="40px"/>
-//           </li>
-//           <li className={styles.li}>
-//             <a className={window.location.pathname === '/lohin' ?
-//             `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/login">Login</a>
-//             <img src= { Profile } alt="profile" height="40px" width="40px"/>
-//           </li>
-//       </ul>
-//   );
-//   let loggedIn = (
-  // <ul className={styles.navbar}>
-  //   <li className={styles.li}>
-  //     <a className={window.location.pathname === '/garden' ?
-  //     `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/garden">My garden</a>
-  //     <img src={Leaf} alt="leaf" height="40px" width="40px" />
-  //   </li>
-  //   <li className={styles.li}>
-  //     <a className={window.location.pathname === '/users' ?
-  //     `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/users">Users</a>
-  //     <img src={Users} alt="users" height="40px" width="40px" />
-  //   </li>
-  //   <li className={styles.li}>
-  //     <a className={window.location.pathname === '/events' ?
-  //     `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/events">Events</a>
-  //     <img src={Calendar} alt="calendar" height="40px" width="40px" />
-  //   </li>
-  //   <li className={styles.li}>
-  //     <a className={window.location.pathname === '/plant' ?
-  //     `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/plant">Plants</a>
-  //     <img src={Search} alt="search" height="40px" width="40px" />
-  //   </li>
-  //   <li className={styles.li}>
-  //     <a className={window.location.pathname === '/chat' ?
-  //     `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/chat">Chat</a>
-  //     <img src={Chat} alt="chat" height="40px" width="40px" />
-  //   </li>
-  //   <li className={styles.li}>
-  //     <a className={window.location.pathname === '/login' ?
-  //     `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/login">Login</a>
-  //     <img src={Profile} alt="profile" height="40px" width="40px" />
-  //   </li>
-  // </ul>
-//   );
+function LoggedOut() {
+  return (
+    <ul className={styles.navbar}>
+      <li className={styles.li}>
+        <a className={window.location.pathname === '/plant' ? `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/plant">Plants</a>
+        <img src={Search} alt="search" height="40px" width="40px" />
+      </li>
+      <li className={styles.li}>
+        <a className={window.location.pathname === '/chat' ? `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/chat">Chat</a>
+        <img src={Chat} alt="chat" height="40px" width="40px" />
+      </li>
+      <li className={styles.li}>
+        <a className={window.location.pathname === '/login' ? `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/login">Login</a>
+        <img src={Profile} alt="profile" height="40px" width="40px" />
+      </li>
+    </ul>
+  );
+}
 
+function LoggedIn() {
   return (
     <ul className={styles.navbar}>
       <li className={styles.li}>
@@ -86,11 +51,20 @@ function Navbar() {
         <img src={Chat} alt="chat" height="40px" width="40px" />
       </li>
       <li className={styles.li}>
-        <a className={window.location.pathname === '/login' ? `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/login">Login</a>
+        <a className={window.location.pathname === '/myprofile' ? `${styles.link} ${styles.clicked}` : `${styles.link}`} href="/myprofile">login</a>
         <img src={Profile} alt="profile" height="40px" width="40px" />
       </li>
     </ul>
   );
+}
+
+function Navbar(props) {
+  // eslint-disable-next-line react/prop-types
+  const { isLoggedIn } = props;
+  if (isLoggedIn) {
+    return <LoggedIn />;
+  }
+  return <LoggedOut />;
 }
 
 export default Navbar;
