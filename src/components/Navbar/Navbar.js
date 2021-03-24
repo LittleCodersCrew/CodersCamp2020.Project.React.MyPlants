afe4f48b6c86bbd5e6826b962091653fd140976f
Weronika Brzęczkowska <brzeczkowska.w@gmail.com>
2021-03-24 13:03:07 +0100

    changes in Navbar component function added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,25 +4,77 @@ import React from 'react';
 import Search from '../../assets/icons/Search.png';
 import Chat from '../../assets/icons/Chat.png';
 import Profile from '../../assets/icons/Profile.png';
+import Users from '../../assets/icons/3 User.png';
+import Calendar from '../../assets/icons/Calendar.png';
+import Leaf from '../../assets/icons/Leaf.png';
 
 function Navbar() {
-//   let loggedIn = (
+//   let loggedOut = (
 //       <ul className="nav-bar">
-//           <li>
-//             <a className="navbar-link" href="/plant">Plants</a>
+//           <li className="nav-li">
+//             <a className="navbar-link" href="#plant">Plants</a>
 //             <img src= { Search } alt="search" height="40px" width="40px"/>
 //           </li>
+//           <li className="nav-li">
+//             <a className="navbar-link" href="#chat">Chat</a>
+//             <img className="navbar-image" src= { Chat } alt="chat" height="40px" width="40px"/>
+//           </li>
+//           <li className="nav-li">
+//             <a className="navbar-link" href="#login">Login</a>
+//             <img src= { Profile } alt="profile" height="40px" width="40px"/>
+//           </li>
 //       </ul>
-//   )
+//   );
+//   let loggedIn = (
+//     <ul className="nav-bar">
+//       <li className="nav-li">
+//         <a className="navbar-link" href="#garden">My garden</a>
+//         <img className="navbar-image" src= { Leaf } alt="leaf" height="40px" width="40px"/>
+//       </li>
+//       <li className="nav-li">
+//         <a className="navbar-link" href="#users">Users</a>
+//         <img className="navbar-image" src= { Users } alt="users" height="40px" width="40px"/>
+//       </li>
+//       <li className="nav-li">
+//         <a className="navbar-link" href="#events">Events</a>
+//         <img className="navbar-image" src= { Calendar } alt="calendar" height="40px" width="40px"/>
+//       </li>
+//       <li className="nav-li">
+//         <a className="navbar-link" href="#plant">Plants</a>
+//         <img src= { Search } alt="search" height="40px" width="40px"/>
+//       </li>
+//       <li className="nav-li">
+//         <a className="navbar-link" href="#chat">Chat</a>
+//         <img className="navbar-image" src= { Chat } alt="chat" height="40px" width="40px"/>
+//       </li>
+//       <li className="nav-li">
+//         <a className="navbar-link" href="#login">Login</a>
+//         <img src= { Profile } alt="profile" height="40px" width="40px"/>
+//       </li>
+//    </ul>
+//   );
+
   return (
     <ul className="nav-bar">
+          <li className="nav-li">
+            <a className="navbar-link" href="#garden">My garden</a>
+            <img className="navbar-image" src= { Leaf } alt="leaf" height="40px" width="40px"/>
+          </li>
+          <li className="nav-li">
+            <a className="navbar-link" href="#users">Users</a>
+            <img className="navbar-image" src= { Users } alt="users" height="40px" width="40px"/>
+          </li>
+          <li className="nav-li">
+            <a className="navbar-link" href="#events">Events</a>
+            <img className="navbar-image" src= { Calendar } alt="calendar" height="40px" width="40px"/>
+          </li>
           <li className="nav-li">
             <a className="navbar-link" href="#plant">Plants</a>
             <img src= { Search } alt="search" height="40px" width="40px"/>
           </li>
           <li className="nav-li">
             <a className="navbar-link" href="#chat">Chat</a>
-            <img src= { Chat } alt="chat" height="40px" width="40px"/>
+            <img className="navbar-image" src= { Chat } alt="chat" height="40px" width="40px"/>
           </li>
           <li className="nav-li">
             <a className="navbar-link" href="#login">Login</a>
