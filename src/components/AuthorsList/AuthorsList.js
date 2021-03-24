8a93ccbde895a0b43edd6d177524e7ad417e5242
Kamila Grusza <kamila.grusza@tlen.pl>
2021-03-24 10:36:25 +0100

    change styles

--- src/components/AuthorsList/AuthorsList.js
+++ src/components/AuthorsList/AuthorsList.js
@@ -1,16 +1,15 @@
-import './AuthorsList.scss';
-import React from 'react';
+import styles from './AuthorsList.scss';
 
 function AuthorsList() {
   return (
-    <div className="authors">
-      <a className="link" href="https://github.com/adax10" target="_blank" rel="noreferrer">Adrianna Krupa</a>
-      <a className="link" href="https://github.com/Suegro24" target="_blank" rel="noreferrer">Dominik Puchała</a>
-      <a className="link" href="https://github.com/Nilphym" target="_blank" rel="noreferrer">Jędrzej Ratajczak</a>
-      <a className="link" href="https://github.com/kami3la" target="_blank" rel="noreferrer">Kamila Grusza</a>
-      <a className="link" href="https://github.com/KonradMierzejewski" target="_blank" rel="noreferrer">Konrad Mierzejewski</a>
-      <a className="link" href="https://github.com/brzeczkowskaw" target="_blank" rel="noreferrer">Weronika Brzęczkowska-Kuzianik</a>
-      <a className="link link--mentor" href="https://github.com/ruljin" target="_blank" rel="noreferrer">Filip Kuca - mentor</a>
+    <div className={styles.authors}>
+      <a className={styles.link} href="https://github.com/adax10" target="_blank" rel="noreferrer">Adrianna Krupa</a>
+      <a className={styles.link} href="https://github.com/Suegro24" target="_blank" rel="noreferrer">Dominik Puchała</a>
+      <a className={styles.link} href="https://github.com/Nilphym" target="_blank" rel="noreferrer">Jędrzej Ratajczak</a>
+      <a className={styles.link} href="https://github.com/kami3la" target="_blank" rel="noreferrer">Kamila Grusza</a>
+      <a className={styles.link} href="https://github.com/KonradMierzejewski" target="_blank" rel="noreferrer">Konrad Mierzejewski</a>
+      <a className={styles.link} href="https://github.com/brzeczkowskaw" target="_blank" rel="noreferrer">Weronika Brzęczkowska-Kuzianik</a>
+      <a className={(styles.link, styles.link--mentor)} href="https://github.com/ruljin" target="_blank" rel="noreferrer">Filip Kuca - mentor</a>
     </div>
   );
 }
