53519dc40882cf526751fcc89de52d0437d36a77
Weronika Brzęczkowska <brzeczkowska.w@gmail.com>
2021-03-24 13:31:52 +0100

    styles changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,16 @@
-import './Footer.scss';
+import styles from './Footer.module.scss';
 import React from 'react';
 
 import Heart from '../../assets/icons/Heart.png';
 
 function Footer() {
   return (
-    <footer className="footer-container">
-      <div className="link-container">
+    <footer className={styles.footer}>
+      <div className={styles.container}>
         <img src={Heart} alt="heart" height="40px" width="40px" />
-        <a className="footer-link" href="#authors">Authors</a>
+        <a className={styles.link} href="#authors">Authors</a>
       </div>
-      <p className="copyright">©2021 Copyright</p>
+      <p className={styles.copyright}>©2021 Copyright</p>
     </footer>
   );
 }
