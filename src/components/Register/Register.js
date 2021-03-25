f018fc114c5e6afc795c19ce3e8e5bc0c1ba1c2e
Konrad <k.mierzejewski07@gmail.com>
2021-03-25 12:16:28 +0100

    added functionality, fixed styling

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Register.module.scss';
 import Logo from '../../assets/logo.png';
 
 function Register() {
+  const [value, setValue] = useState({
+    name: '',
+    surname: '',
+    login: '',
+    email: '',
+    password: '',
+    repeatPassword: ''
+  });
+
+  const inputHandler = (e) => {
+    setValue({ [e.target.name]: e.target.value });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.login}>
-      <form>
+      <form onChange={submit}>
         <div className={styles.logo}>
           <img src={Logo} alt="myplants logo" />
         </div>
         <div className={styles.formInput}>
-          <input className={styles.input} placeholder="Name" />
+          <input className={styles.input} placeholder="Name" onChange={inputHandler} value={value.name} />
         </div>
         <div className={styles.formInput}>
-          <input className={styles.input} placeholder="Surname" />
+          <input className={styles.input} placeholder="Surname" onChange={inputHandler} value={value.surname} />
         </div>
         <div className={styles.formInput}>
-          <input className={styles.input} placeholder="Login" />
+          <input className={styles.input} placeholder="Login" onChange={inputHandler} value={value.login} />
         </div>
         <div className={styles.formInput}>
-          <input className={styles.input} type="email" placeholder="E-mail" />
+          <input className={styles.input} type="email" placeholder="E-mail" onChange={inputHandler} value={value.email} />
         </div>
         <div className={styles.formInput}>
-          <input className={styles.input} placeholder="Password" />
+          <input className={styles.input} type="password" placeholder="Password" onChange={inputHandler} value={value.password} />
         </div>
         <div className={styles.formInput}>
-          <input className={styles.input} placeholder="Repeat password" />
+          <input className={styles.input} type="password" placeholder="Repeat password" onChange={inputHandler} value={value.repeatPassword} />
         </div>
         <div className={styles.buttons}>
           <input className={styles.btn} type="submit" value="Register" />
