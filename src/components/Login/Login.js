527063dc9e99799a99dfc615ea2e5e29a2d29ba7
Konrad <k.mierzejewski07@gmail.com>
2021-03-25 12:08:12 +0100

    added functionality, fixed styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Login.module.scss';
 import Logo from '../../assets/logo.png';
 
 function Login() {
+  const [value, setValue] = useState({
+    email: '',
+    password: ''
+  });
+
+  const inputHandler = (e) => {
+    setValue({ [e.target.name]: e.target.value });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.login}>
-      <form>
+      <form onChange={submit}>
         <div className={styles.logo}>
           <img src={Logo} alt="myplants logo" />
         </div>
         <div className={styles.formInput}>
-          <input className={styles.input} type="email" id="email" placeholder="E-mail" />
+          <input className={styles.input} type="email" id="email" placeholder="E-mail" onChange={inputHandler} value={value.email} />
         </div>
         <div className={styles.formInput}>
-          <input className={styles.input} type="password" id="password" placeholder="Password" />
+          <input className={styles.input} type="password" id="password" placeholder="Password" onChange={inputHandler} value={value.password} />
         </div>
         <div className={styles.buttons}>
           <input className={styles.btn} type="submit" value="Login" />
