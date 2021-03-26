d32c2de0dbe5051034a7f388e260e37143ec8b30
unknown <nilphym@gmail.com>
2021-03-26 11:17:55 +0100

    Add semantic tag to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -23,22 +23,26 @@ const createItem = (path, location, linkName, src, alt) => {
 };
 
 const LoggedOut = ({ location }) => (
-  <ul className={navbar}>
-    {createItem('/plant', location, 'Plants', Search, 'search')}
-    {createItem('/chat', location, 'Chat', Chat, 'chat')}
-    {createItem('/login', location, 'Login', Profile, 'profile')}
-  </ul>
+  <nav>
+    <ul className={navbar}>
+      {createItem('/plant', location, 'Plants', Search, 'search')}
+      {createItem('/chat', location, 'Chat', Chat, 'chat')}
+      {createItem('/login', location, 'Login', Profile, 'profile')}
+    </ul>
+  </nav>
 );
 
 const LoggedIn = ({ location, name }) => (
-  <ul className={navbar}>
-    {createItem('/garden', location, 'My garden', Leaf, 'leaf')}
-    {createItem('/users', location, 'Users', Users, 'users')}
-    {createItem('/events', location, 'Events', Calendar, 'calendar')}
-    {createItem('/plant', location, 'Plants', Search, 'search')}
-    {createItem('/chat', location, 'Chat', Chat, 'chat')}
-    {createItem('/myprofile', location, name, Profile, 'profile')}
-  </ul>
+  <nav>
+    <ul className={navbar}>
+      {createItem('/garden', location, 'My garden', Leaf, 'leaf')}
+      {createItem('/users', location, 'Users', Users, 'users')}
+      {createItem('/events', location, 'Events', Calendar, 'calendar')}
+      {createItem('/plant', location, 'Plants', Search, 'search')}
+      {createItem('/chat', location, 'Chat', Chat, 'chat')}
+      {createItem('/myprofile', location, name, Profile, 'profile')}
+    </ul>
+  </nav>
 );
 
 const Navbar = ({ name, location }) => (
