c90a13455a688287b3613fae434c65830511886c
unknown <nilphym@gmail.com>
2021-03-26 10:53:33 +0100

    Add template routes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,36 @@ import { Switch, Route, useLocation } from 'react-router-dom';
 import './App.scss';
 import Navbar from './components/Navbar';
 
-function About() {
+function Home() {
   return <h2>About</h2>;
+}
+
+function Plants() {
+  return <h2>Plants</h2>;
+}
+
+function Chat() {
+  return <h2>Chat</h2>;
+}
+
+function Login() {
+  return <h2>Login</h2>;
+}
+
+function Garden() {
+  return <h2>Garden</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function Calendar() {
+  return <h2>Calendar</h2>;
+}
+
+function Profile() {
+  return <h2>Profile</h2>;
 }
 
 const App = () => {
@@ -16,8 +44,29 @@ const App = () => {
     <>
       <Navbar name={userName} location={location} />
       <Switch>
-        <Route path="/about" exact>
-          <About />
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/plant" exact>
+          <Plants />
+        </Route>
+        <Route path="/chat" exact>
+          <Chat />
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/garden" exact>
+          <Garden />
+        </Route>
+        <Route path="/users" exact>
+          <Users />
+        </Route>
+        <Route path="/events" exact>
+          <Calendar />
+        </Route>
+        <Route path="/myprofile" exact>
+          <Profile />
         </Route>
       </Switch>
     </>
