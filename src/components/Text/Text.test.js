a0c0f4b034c6a6a1941ffa9606c08d1429bb9977
Weronika <brzeczkowska.w@gmail.com>
2021-03-28 20:08:35 +0200

    Correct tests

--- src/components/Text/Text.test.js
+++ src/components/Text/Text.test.js
@@ -5,15 +5,18 @@ import Text from './Text';
 describe('Text component render', () => {
   it('Gives the right text inside paragraph', () => {
     const words = 'Hello World';
+    const font = '80px';
 
-    render(<Text text="Hello World" />);
+    render(<Text text={words} fontsize={font} />);
 
     expect(screen.getByText(words)).toBeInTheDocument();
   });
 
   it('Uses the choosen font size', () => {
     const text = 'Hello World';
-    render(<Text text={text} fontsize="80px" />);
+    const font = '80px';
+
+    render(<Text text={text} fontsize={font} />);
 
     expect(screen.getByText(text)).toHaveAttribute('style', 'font-size: 80px;');
   });
