a798ca7c5d6116ee03eaeb99ebea2fade2074a63
Ada Krupa <adriannakrupa01@gmail.com>
2021-03-28 21:08:20 +0200

    Add onClick as props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,22 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { button } from './Button.module.scss';
 
-const Button = ({ type, text }) => {
+const Button = ({ type, text, onClick }) => {
   if (type === 'submit') {
     return (
-      <button className={button} type="submit">
+      <button className={button} type="submit" onClick={onClick}>
         {text}
       </button>
     );
   }
 
   return (
-    <button className={button} type="button">
+    <button className={button} type="button" onClick={onClick}>
       {text}
     </button>
   );
 };
 
-Button.propTypes = { type: PropTypes.oneOf(['button', 'submit']), text: PropTypes.string };
-Button.defaultProps = { type: 'button', text: '' };
+Button.propTypes = {
+  type: PropTypes.oneOf(['button', 'submit']),
+  onClick: PropTypes.func,
+  text: PropTypes.string.isRequired
+};
+Button.defaultProps = { type: 'button', onClick: () => null };
 export default Button;
