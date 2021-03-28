cabcb44c3fdbd8e3434b22a5ea2f2d3136fe7e63
unknown <nilphym@gmail.com>
2021-03-28 22:49:36 +0200

    Fix button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,26 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { button } from './Button.module.scss';
 
-const Button = ({ type, text, onClick }) => {
-  if (type === 'submit') {
-    return (
-      <button className={button} type="submit" onClick={onClick}>
-        {text}
-      </button>
-    );
-  }
-
-  return (
-    <button className={button} type="button" onClick={onClick}>
-      {text}
-    </button>
-  );
-};
+const Button = ({ type, text, onClick }) => (
+  <button className={button} type={type} onClick={onClick}>
+    {text}
+  </button>
+);
 
 Button.propTypes = {
   type: PropTypes.oneOf(['button', 'submit']),
-  onClick: PropTypes.func,
+  onClick: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired
 };
-Button.defaultProps = { type: 'button', onClick: () => null };
+Button.defaultProps = { type: 'button' };
 export default Button;
