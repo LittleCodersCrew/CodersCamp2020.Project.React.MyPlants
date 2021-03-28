e54f3c578ec06b80f94f06fc510e74bb30f1d497
unknown <nilphym@gmail.com>
2021-03-28 16:30:57 +0200

    Remove selector from index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './App';
-import Select from './components/Select/Select';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App />
-      <Select title="watering" values={['aaa', 'bbb', 'ccc']} />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
