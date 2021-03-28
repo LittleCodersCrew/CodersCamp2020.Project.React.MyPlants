01b877d944ebcdeaebaf6bcf698563fea07ec0c7
Weronika <brzeczkowska.w@gmail.com>
2021-03-28 15:41:55 +0200

    Change import route of container

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './App';
-import Text from './components/Text/index';
+import Text from './components/Text';
 
 ReactDOM.render(
   <React.StrictMode>
