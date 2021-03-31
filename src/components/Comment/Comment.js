e8d77da9bd5c161609f1751c8275d07f62d19119
unknown <nilphym@gmail.com>
2021-03-31 23:32:54 +0200

    Add comment component

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classes from './comment.module.scss';
+import Avatar from '../../assets/icons/3 User.png';
+import Star from '../../assets/icons/Star.png';
+
+const Comment = ({ username, dateTime, content, likesCount, likeHandler, isLiked }) => (
+  <div className={classes.comment}>
+    <div className={classes.user}>
+      <img src={Avatar} alt="User avatar" />
+      <p className={classes.userInfo}>
+        <span>{username}</span>
+        <span>{dateTime}</span>
+      </p>
+    </div>
+    <p className={classes.content}>{content}</p>
+    <div className={classes.footer}>
+      <p className={classes.likes}>
+        <img className={classes.star} src={Star} alt="Star symbol" />
+        <span>
+          {likesCount}
+          {' '}
+          {likesCount === 1 ? 'like' : 'likes'}
+        </span>
+      </p>
+      {
+        likeHandler === null ? ''
+          : (
+            <button className={classes.btn} onClick={likeHandler}>
+              {isLiked ? 'Don\'t like it' : 'Like it!'}
+            </button>
+          )
+      }
+    </div>
+  </div>
+);
+
+export default Comment;
+
+Comment.propTypes = {
+  username: PropTypes.string.isRequired,
+  dateTime: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  likesCount: PropTypes.string.isRequired,
+  likeHandler: PropTypes.func,
+  isLiked: PropTypes.bool
+};
+
+Comment.defaultProps = {
+  likeHandler: null,
+  isLiked: false
+};
