aa7f2bdb77c2348e24d0057e96551420b4f9e29d
Weronika <brzeczkowska.w@gmail.com>
2021-04-01 21:31:07 +0200

    Add buttons to Note-component

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { noteContainer, mainText, signText } from './Note.module.scss';
+import { noteContainer, mainText, signText, sign, buttons } from './Note.module.scss';
+import SmallButton from '../SmallButton/SmallButton';
 
 const Note = ({ noteText, noteDate }) => (
   <div className={noteContainer}>
     <p className={mainText}>{noteText}</p>
-    <p className={signText}>{noteDate}</p>
+    <div className={sign}>
+      <p className={signText}>{noteDate}</p>
+      <div className={buttons}>
+        <SmallButton text="Delete" fontsize="0.7rem" />
+        <SmallButton text="Edit" fontsize="0.7rem" />
+      </div>
+    </div>
+
   </div>
 );
 
