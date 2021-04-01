424707559fdb415307f862a0a2d408f6e601c4b3
Weronika <brzeczkowska.w@gmail.com>
2021-04-02 00:16:34 +0200

    Add scrollbars

--- src/containers/UserPage/UserPage.js
+++ src/containers/UserPage/UserPage.js
@@ -9,7 +9,7 @@ import UserProfile from '../../components/UserProfile';
 import Star from '../../assets/icons/Star.png';
 import profileleaf from '../../assets/illustrations/plant-leaf.png';
 
-import { mainContainer, header, journal, garden, friends, items } from './UserPage.module.scss';
+import { mainContainer, header, journal, garden, friends, plants, users, notes } from './UserPage.module.scss';
 
 const notetext = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 const date = '01.04.2021.   20.09';
@@ -26,14 +26,18 @@ const UserPage = () => (
     </header>
     <div className={journal}>
       <Text text="Login's journal" fontsize="1.5em" />
-      <Note noteText={notetext} noteDate={date} />
-      <Note noteText={notetext} noteDate={date} />
-      <Note noteText={notetext} noteDate={date} />
-      <SmallButton text="Load more" fontsize="1.5em" />
+      <div className={notes}>
+        <Note noteText={notetext} noteDate={date} />
+        <Note noteText={notetext} noteDate={date} />
+        <Note noteText={notetext} noteDate={date} />
+        <Note noteText={notetext} noteDate={date} />
+        <Note noteText={notetext} noteDate={date} />
+        <SmallButton text="Load more" fontsize="1.5em" />
+      </div>
     </div>
     <div className={garden}>
       <Text text="Login's garden" fontsize="1.5em" />
-      <div className={items}>
+      <div className={plants}>
         <PlantProfile name="Name" image={profileleaf} />
         <PlantProfile name="Name" image={profileleaf} />
         <PlantProfile name="Name" image={profileleaf} />
@@ -44,7 +48,7 @@ const UserPage = () => (
     </div>
     <div className={friends}>
       <Text text="Login's favorite users" fontsize="1.5em" />
-      <div className={items}>
+      <div className={users}>
         <UserProfile name="User" />
         <UserProfile name="User" />
         <SmallButton text="+ 17 more" fontsize="1.5em" />
