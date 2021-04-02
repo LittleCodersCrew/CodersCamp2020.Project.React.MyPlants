935cf4fe1f4bec35710acb99bf3e750b130b0410
Ada Krupa <adriannakrupa01@gmail.com>
2021-04-02 14:43:28 +0200

    Change the tag a to link

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
 import styles, { error } from './Login.module.scss';
@@ -73,7 +74,7 @@ function Login({ setToken }) {
             Login
           </button>
           <button type="button" className={styles.btn}>
-            <a href="/register">Register</a>
+            <Link to="/register">Register</Link>
           </button>
         </div>
       </form>
