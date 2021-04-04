9e1282a2cf2cffe449faacaa40b36bfe89f4d5e2
Konrad <k.mierzejewski07@gmail.com>
2021-04-04 13:30:34 +0200

    Fix styles and its import

--- src/containers/Modal/Modal.js
+++ src/containers/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PlantInfo from '../../components/PlantInfo/PlantInfo';
 import samplePlant from '../../assets/illustrations/plant.png';
-import { modal, summary, userMessage, message } from './Modal.module.scss';
+import styles from './Modal.module.scss';
 import Button from '../../components/Button/Button';
 
 const plantDetails = {
@@ -21,19 +21,19 @@ const plantDetails = {
 };
 
 const Modal = () => (
-  <div className={modal}>
+  <div className={styles.modal}>
     <PlantInfo name="Chinese Money" latinName="Pilea peperomides" plantDetails={plantDetails} image={samplePlant} />
-    <div className={summary}>
-      <Button className="button" text="Edit" />
-      <Button className="button" text="Delete" />
+    <div className={styles.summary}>
+      <Button className={styles.button} text="Edit" />
+      <Button className={styles.button} text="Delete" />
     </div>
-    <div className={userMessage}>
-      <h1 className={message}>
+    <div className={styles.userMessage}>
+      <h1 className={styles.message}>
         Thank you for your participation!
         <br />
         As soon as it is verified by our team, this plant will be available for others in our base.
       </h1>
-      <Button className="button" text="Save" />
+      <Button className={styles.button} text="Save" />
     </div>
   </div>
 );
