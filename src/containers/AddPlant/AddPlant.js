e3abc64fa38c738ee8275711da0ddd2884aa28d3
Konrad <k.mierzejewski07@gmail.com>
2021-04-04 13:18:54 +0200

    Fix styles import.

--- src/containers/AddPlant/AddPlant.js
+++ src/containers/AddPlant/AddPlant.js
@@ -1,50 +1,50 @@
 import React from 'react';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
-import { container, basicInfo, detailedInfo, input, select, title, button, addPicture, plantPicture, removePicture, addBtn, submit, submitBtn } from './AddPlant.module.scss';
+import styles from './AddPlant.module.scss';
 import PlantPic from '../../assets/illustrations/plant-leaf.png';
 import RemovePic from '../../assets/icons/Paper Fail.png';
 import Button from '../../components/Button/Button';
 
 const AddPlant = () => (
-  <div className={container}>
-    <div className={basicInfo}>
-      <h1 className={title}>
+  <div className={styles.container}>
+    <div className={styles.basicInfo}>
+      <h1 className={styles.title}>
         Help us grow!
         <br />
         If you did not find your plant in our base, you can add it below.
       </h1>
-      <Input className={input} text="Name" />
-      <Input className={input} text="Latin Name" />
-      <Select className={select} title="Species" />
+      <Input className={styles.input} text="Name" />
+      <Input className={styles.input} text="Latin Name" />
+      <Select className={styles.select} title="Species" />
     </div>
-    <div className={detailedInfo}>
-      <Select className={select} title="Min temperature" />
-      <Select className={select} title="Humidity" />
-      <Select className={select} title="Watering" />
-      <Select className={select} title="Application" />
-      <Select className={select} title="Max Temperature" />
-      <Select className={select} title="Sunlight" />
-      <Select className={select} title="Watering Method" />
-      <Select className={select} title="Subsoil" />
-      <Select className={select} title="Conditioners" />
-      <Select className={select} title="Spraying" />
-      <Select className={select} title="Toxicity" />
-      <Select className={select} title="Animals at home?" />
+    <div className={styles.detailedInfo}>
+      <Select className={styles.select} title="Min temperature" />
+      <Select className={styles.select} title="Humidity" />
+      <Select className={styles.select} title="Watering" />
+      <Select className={styles.select} title="Application" />
+      <Select className={styles.select} title="Max Temperature" />
+      <Select className={styles.select} title="Sunlight" />
+      <Select className={styles.select} title="Watering Method" />
+      <Select className={styles.select} title="Subsoil" />
+      <Select className={styles.select} title="Conditioners" />
+      <Select className={styles.select} title="Spraying" />
+      <Select className={styles.select} title="Toxicity" />
+      <Select className={styles.select} title="Animals at home?" />
     </div>
-    <div className={addPicture}>
-      <div className={plantPicture}>
+    <div className={styles.addPicture}>
+      <div className={styles.plantPicture}>
         <img src={PlantPic} alt="sample plant" style={{ height: '100px', width: '100px' }} />
       </div>
-      <div className={removePicture}>
+      <div className={styles.removePicture}>
         <img src={RemovePic} alt="remove" style={{ height: '25px', width: '25px' }} />
       </div>
-      <div className={addBtn}>
-        <Button className={button} text="Add picture" />
+      <div className={styles.addBtn}>
+        <Button className={styles.button} text="Add picture" />
       </div>
     </div>
-    <div className={submit}>
-      <Button className={submitBtn} text="Add your plant" />
+    <div className={styles.submit}>
+      <Button className={styles.submitBtn} text="Add your plant" />
     </div>
   </div>
 );
