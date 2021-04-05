8d47d4761364257ec2539e09fc714c5d75aa18c3
Konrad <k.mierzejewski07@gmail.com>
2021-04-05 13:53:24 +0200

    Add show/hide modal on click

--- src/containers/AddPlant/AddPlant.js
+++ src/containers/AddPlant/AddPlant.js
@@ -1,52 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
 import styles from './AddPlant.module.scss';
 import PlantPic from '../../assets/illustrations/plant-leaf.png';
 import RemovePic from '../../assets/icons/Paper Fail.png';
 import Button from '../../components/Button/Button';
+import Text from '../../components/Text/Text';
+import Modal from '../Modal/Modal';
 
-const AddPlant = () => (
-  <div className={styles.container}>
-    <div className={styles.basicInfo}>
-      <h1 className={styles.title}>
-        Help us grow!
-        <br />
-        If you did not find your plant in our base, you can add it below.
-      </h1>
-      <Input className={styles.input} text="Name" />
-      <Input className={styles.input} text="Latin Name" />
-      <Select className={styles.select} title="Species" />
-    </div>
-    <div className={styles.detailedInfo}>
-      <Select className={styles.select} title="Min temperature" />
-      <Select className={styles.select} title="Humidity" />
-      <Select className={styles.select} title="Watering" />
-      <Select className={styles.select} title="Application" />
-      <Select className={styles.select} title="Max Temperature" />
-      <Select className={styles.select} title="Sunlight" />
-      <Select className={styles.select} title="Watering Method" />
-      <Select className={styles.select} title="Subsoil" />
-      <Select className={styles.select} title="Conditioners" />
-      <Select className={styles.select} title="Spraying" />
-      <Select className={styles.select} title="Toxicity" />
-      <Select className={styles.select} title="Animals at home?" />
-    </div>
-    <div className={styles.addPicture}>
-      <div className={styles.plantPicture}>
-        <img src={PlantPic} alt="sample plant" style={{ height: '100px', width: '100px' }} />
+const AddPlant = () => {
+  const [show, setShow] = useState(false);
+  const openModal = () => setShow(true);
+  const closeModal = () => setShow(false);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.basicInfo}>
+        <div className={styles.title}>
+          <Text text="Help us grow!" fontsize="2em" />
+          <Text text="If you did not find your plant in our base, you can add it below." fontsize="2em" />
+        </div>
+        <Modal closeModal={closeModal} show={show} />
+        <Input className={styles.input} text="Name" />
+        <Input className={styles.input} text="Latin Name" />
+        <Select className={styles.select} title="Species" />
       </div>
-      <div className={styles.removePicture}>
-        <img src={RemovePic} alt="remove" style={{ height: '25px', width: '25px' }} />
+      <div className={styles.detailedInfo}>
+        <Select className={styles.select} title="Min temperature" values={[10, 15, 20]} />
+        <Select className={styles.select} title="Humidity" />
+        <Select className={styles.select} title="Watering" />
+        <Select className={styles.select} title="Application" />
+        <Select className={styles.select} title="Max Temperature" />
+        <Select className={styles.select} title="Sunlight" />
+        <Select className={styles.select} title="Watering Method" />
+        <Select className={styles.select} title="Subsoil" />
+        <Select className={styles.select} title="Conditioners" />
+        <Select className={styles.select} title="Spraying" />
+        <Select className={styles.select} title="Toxicity" />
+        <Select className={styles.select} title="Animals at home?" />
       </div>
-      <div className={styles.addBtn}>
-        <Button className={styles.button} text="Add picture" />
+      <div className={styles.addPicture}>
+        <div className={styles.plantPicture}>
+          <img src={PlantPic} alt="sample plant" style={{ height: '100px', width: '100px' }} />
+        </div>
+        <div className={styles.removePicture}>
+          <img src={RemovePic} alt="remove" style={{ height: '25px', width: '25px' }} />
+        </div>
+        <div className={styles.addBtn}>
+          <Button className={styles.button} text="Add picture" />
+        </div>
+      </div>
+      <div className={styles.submit}>
+        {!show && <Button className={styles.button} text="Add your plant" onClick={openModal} />}
       </div>
     </div>
-    <div className={styles.submit}>
-      <Button className={styles.submitBtn} text="Add your plant" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default AddPlant;
--- src/containers/Modal/Modal.js
+++ src/containers/Modal/Modal.js
@@ -3,6 +3,7 @@ import PlantInfo from '../../components/PlantInfo/PlantInfo';
 import samplePlant from '../../assets/illustrations/plant.png';
 import styles from './Modal.module.scss';
 import Button from '../../components/Button/Button';
+import Text from '../../components/Text/Text';
 
 const plantDetails = {
   species: 'Plant',
@@ -20,22 +21,26 @@ const plantDetails = {
   safeForAnimals: true
 };
 
-const Modal = () => (
-  <div className={styles.modal}>
-    <PlantInfo name="Chinese Money" latinName="Pilea peperomides" plantDetails={plantDetails} image={samplePlant} />
-    <div className={styles.summary}>
-      <Button className={styles.button} text="Edit" />
-      <Button className={styles.button} text="Delete" />
+const Modal = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { show, closeModal } = props;
+
+  return (
+    <div className={show ? 'modal' : 'hide'}>
+      <div className={styles.modal}>
+        <PlantInfo name="Chinese Money" latinName="Pilea peperomides" plantDetails={plantDetails} image={samplePlant} />
+        <div className={styles.summary}>
+          <Button className={styles.button} text="Edit" />
+          <Button className={styles.button} text="Delete" />
+        </div>
+        <div className={styles.userMessage}>
+          <Text text="Thank you for your participation!" fontsize="1.5em" />
+          <Text text="As soon as it is verified by our team, this plant will be available for others in our base." fontsize="1.5em" />
+        </div>
+        <Button className={styles.button} text="Save" onClick={closeModal} />
+      </div>
     </div>
-    <div className={styles.userMessage}>
-      <h1 className={styles.message}>
-        Thank you for your participation!
-        <br />
-        As soon as it is verified by our team, this plant will be available for others in our base.
-      </h1>
-      <Button className={styles.button} text="Save" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Modal;
