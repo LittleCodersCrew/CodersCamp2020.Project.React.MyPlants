c88e1143b6a09dc6cea682905528ff1c03691362
Konrad <k.mierzejewski07@gmail.com>
2021-04-10 18:05:54 +0200

    Fix styles import

--- src/containers/AddPlant/AddPlant.js
+++ src/containers/AddPlant/AddPlant.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
-import styles from './AddPlant.module.scss';
+import { container, title, basicInfo, detailedInfo, addPicture, plantPicture, removePicture, addBtn, submit } from './AddPlant.module.scss';
 import PlantPic from '../../assets/illustrations/plant-leaf.png';
 import RemovePic from '../../assets/icons/Paper Fail.png';
 import Button from '../../components/Button';
@@ -14,17 +14,17 @@ const AddPlant = () => {
   const closeModal = () => setShow(false);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>
+    <div className={container}>
+      <div className={title}>
         <Text text="Help us grow!" fontsize="2em" />
         <Text text="If you did not find your plant in our base, you can add it below." fontsize="2em" />
       </div>
-      <div className={styles.basicInfo}>
+      <div className={basicInfo}>
         <Input text="Name" />
         <Input text="Latin Name" />
         <Select title="Species" />
       </div>
-      <div className={styles.detailedInfo}>
+      <div className={detailedInfo}>
         <Select title="Min temperature" values={[0, 5, 10]} />
         <Select title="Humidity" values={['moderately moist soil', 'moist soil', 'dry']} />
         <Select title="Watering" values={['regulary but not intensively', 'often', 'rarely']} />
@@ -38,18 +38,18 @@ const AddPlant = () => {
         <Select title="Toxicity" />
         <Select title="Animals at home?" />
       </div>
-      <div className={styles.addPicture}>
-        <div className={styles.plantPicture}>
+      <div className={addPicture}>
+        <div className={plantPicture}>
           <img src={PlantPic} alt="sample plant" style={{ height: '100px', width: '100px' }} />
         </div>
-        <div className={styles.removePicture}>
+        <div className={removePicture}>
           <img src={RemovePic} alt="remove" style={{ height: '25px', width: '25px' }} />
         </div>
-        <div className={styles.addBtn}>
+        <div className={addBtn}>
           <Button text="Add picture" />
         </div>
       </div>
-      <div className={styles.submit}>
+      <div className={submit}>
         {!show && <Button text="Add your plant" onClick={openModal} />}
         <Modal closeModal={closeModal} show={show} />
       </div>
--- src/containers/Modal/Modal.js
+++ src/containers/Modal/Modal.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PlantInfo from '../../components/PlantInfo/PlantInfo';
 import samplePlant from '../../assets/illustrations/plant.png';
-import styles from './Modal.module.scss';
 import Button from '../../components/Button/Button';
 import Text from '../../components/Text/Text';
+import { summary, userMessage } from './Modal.module.scss';
 
 const plantDetails = {
   species: 'Plant',
@@ -28,11 +28,11 @@ const Modal = (props) => {
   return (
     <div className={show ? 'modal' : 'hide'}>
       <PlantInfo name="Chinese Money" latinName="Pilea peperomides" plantDetails={plantDetails} image={samplePlant} />
-      <div className={styles.summary}>
+      <div className={summary}>
         <Button text="Edit" />
         <Button text="Delete" />
       </div>
-      <div className={styles.userMessage}>
+      <div className={userMessage}>
         <Text text="Thank you for your participation!" fontsize="1.5em" />
         <Text text="As soon as it is verified by our team, this plant will be available for others in our base." fontsize="1.5em" />
       </div>
