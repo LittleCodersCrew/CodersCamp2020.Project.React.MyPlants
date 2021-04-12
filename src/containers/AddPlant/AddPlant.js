e388c25371e7e5ec0555765fb8e6f4733a0389dc
Konrad <k.mierzejewski07@gmail.com>
2021-04-12 13:29:17 +0200

    Add form and modal

--- src/containers/AddPlant/AddPlant.js
+++ src/containers/AddPlant/AddPlant.js
@@ -7,7 +7,7 @@ import Text from '../../components/Text';
 import Select from '../../components/Select';
 import Button from '../../components/Button';
 
-import { container, title, basicInfo } from './AddPlant.module.scss';
+import { container, title, basicInfo, detailedInfo } from './AddPlant.module.scss';
 
 const AddPlant = () => {
   const { inputs, handleInputChange, handleSubmit } = useSignUpForm();
@@ -24,13 +24,92 @@ const AddPlant = () => {
       </div>
       <form onSubmit={handleSubmit}>
         <div className={basicInfo}>
+          <Modal
+            closeModal={closeModal}
+            show={show}
+            name={inputs}
+            latin={inputs}
+            minTemperature={inputs}
+            humidity={inputs}
+            watering={inputs}
+            application={inputs}
+            maxTemperature={inputs}
+            sunlight={inputs}
+            wateringMethod={inputs}
+            subsoil={inputs}
+            conditioners={inputs}
+            spraying={inputs}
+          />
           <Input text="Name" name="name" onChange={handleInputChange} value={inputs.name} />
           <Input text="Latin Name" name="latinName" onChange={handleInputChange} value={inputs.latinName} />
           <Select title="Species" />
         </div>
+        <div className={detailedInfo}>
+          <Select
+            title="Min temperature"
+            values={[0, 5, 10]}
+            onChange={handleInputChange}
+            value={inputs.minTemp}
+          />
+          <Select
+            title="Humidity"
+            values={['moderately moist soil', 'moist soil', 'dry']}
+            onChange={handleInputChange}
+            value={inputs.humidity}
+          />
+          <Select
+            title="Watering"
+            values={['regulary but not intensively', 'often', 'rarely']}
+            onChange={handleInputChange}
+            value={inputs.watering}
+          />
+          <Select
+            title="Application"
+            values={['decorative', 'edible', 'medical use']}
+            onChange={handleInputChange}
+            value={inputs.application}
+          />
+          <Select
+            title="Max Temperature"
+            values={[25, 30, 35]}
+            onChange={handleInputChange}
+            value={inputs.maxTemp}
+          />
+          <Select
+            title="Sunlight"
+            values={['sun, partial shade, shade', 'sun', 'partial shade', 'shade', 'sun, partial shade']}
+            onChange={handleInputChange}
+            value={inputs.sunlight}
+          />
+          <Select
+            title="Watering Method"
+            values={['to the pot', 'on the saucer or under the leaves']}
+            onChange={handleInputChange}
+            value={inputs.wateringMethod}
+          />
+          <Select
+            title="Subsoil"
+            values={['low soil requirements', 'peat, dertile, fresh, well-drained', 'permeable and well-drained substrate']}
+            onChange={handleInputChange}
+            value={inputs.subsoil}
+          />
+          <Select
+            title="Conditioners"
+            values={['Fertilizer for plants with ornamental leaves', 'mineral fertilizer for potted plants', 'fertilizer for cacti and succulents']}
+            onChange={handleInputChange}
+            value={inputs.conditioners}
+          />
+          <Select
+            title="Spraying"
+            values={['often', 'don\'t need', 'no']}
+            onChange={handleInputChange}
+            value={inputs.spraying}
+          />
+          <Select title="Toxicity" />
+          <Select title="Animals at home?" />
+        </div>
       </form>
       {!show && <Button type="submit" onClick={openModal} text="Add your plant" />}
-      <Modal closeModal={closeModal} show={show} name={inputs} latin={inputs} />
     </div>
   );
 };
--- src/containers/AddPlant/Modal.js
+++ src/containers/AddPlant/Modal.js
@@ -1,18 +1,80 @@
 /* eslint-disable no-console */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { hide, modal } from './AddPlant.module.scss';
-import Input from '../../components/Input';
+import { hide, modal, summary } from './AddPlant.module.scss';
 import Button from '../../components/Button';
 
 const Modal = (props) => {
-  const { show, closeModal, name, latin } = props;
-  console.log(name, latin);
+  const {
+    show,
+    closeModal,
+    name,
+    latin,
+    minTemp,
+    maxTemp,
+    sunlight,
+    humidity,
+    watering,
+    wateringMethod,
+    application,
+    subsoil,
+    conditioners,
+    spraying
+  } = props;
+
   return (
     <div className={show ? modal : hide}>
-      <Input text={name} />
-      <Input text={latin} />
-      <Button onClick={closeModal} text="X" />
+      <div className={summary}>
+        <p>
+          name:
+          {name}
+        </p>
+        <p>
+          latin name:
+          {latin}
+        </p>
+        <p>
+          Min. Temperature:
+          {minTemp}
+        </p>
+        <p>
+          Max. Temperature:
+          {maxTemp}
+        </p>
+        <p>
+          Sunlight:
+          {sunlight}
+        </p>
+        <p>
+          Humidity:
+          {humidity}
+        </p>
+        <p>
+          Watering:
+          {watering}
+        </p>
+        <p>
+          Watering Method:
+          {wateringMethod}
+        </p>
+        <p>
+          Application:
+          {application}
+        </p>
+        <p>
+          Subsoil:
+          {subsoil}
+        </p>
+        <p>
+          Conditioners:
+          {conditioners}
+        </p>
+        <p>
+          Spraying:
+          {spraying}
+        </p>
+      </div>
+      <Button text="Save" onClick={closeModal} />
     </div>
   );
 };
