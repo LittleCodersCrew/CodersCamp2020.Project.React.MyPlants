3c7267b4efa1cfc7727444492bcbb6934a295bc5
Weronika <brzeczkowska.w@gmail.com>
2021-04-12 13:57:34 +0200

    Change view of edit note modal

--- src/components/ModalEditNote/ModalEditNote.js
+++ src/components/ModalEditNote/ModalEditNote.js
@@ -1,15 +1,18 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-underscore-dangle */
-/* eslint-disable no-shadow */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Text from '../Text';
 import Select from '../Select';
+import Button from '../Button';
 import useToken from '../../hooks/useToken/useToken';
 import Database from '../../database';
 import closeSquare from '../../assets/icons/Close Square.png';
-import { hide, overlay, modal, form, input, button } from './ModalEditNote.module.scss';
+import { hide, overlay, modal, form, input, button, tick } from './ModalEditNote.module.scss';
 
 const ModalEditNote = (props) => {
   const { show, closeModal, noteId } = props;
@@ -18,7 +21,8 @@ const ModalEditNote = (props) => {
     image: '',
     title: '',
     text: '',
-    plant: ''
+    plant: '',
+    private: false
   });
   const [myPlants, setMyPlants] = useState([]);
   const [chosenPlant, setChosenPlant] = useState('');
@@ -65,6 +69,12 @@ const ModalEditNote = (props) => {
     return data.json();
   });
 
+  const handleSelectChange = (title, value) => {
+    const choosenPlant1 = myPlants.filter((plant) => `${plant.name}` === value);
+    setChosenPlant(...choosenPlant1);
+    console.log(chosenPlant._id);
+  };
+
   const onSubmit = async (data, e) => {
     e.preventDefault();
     note.plant = chosenPlant._id;
@@ -72,16 +82,10 @@ const ModalEditNote = (props) => {
     console.log(response);
   };
 
-  const handleSelectChange = (title, value) => {
-    const choosenPlant1 = myPlants.filter((plant) => `${plant.name}` === value);
-    setChosenPlant(...choosenPlant1);
-    console.log(chosenPlant._id);
-  };
-
   useEffect(() => {
     fetch(`${Database.URL}/user/${myId}/notes/${noteId}`, {
       headers: {
-        'Content-Type': 'application/',
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`
       }
     }, {})
@@ -91,7 +95,8 @@ const ModalEditNote = (props) => {
           image: json.image,
           title: json.title,
           text: json.text,
-          plant: json.plant
+          plant: json.plant,
+          private: json.private
         })
       );
   });
@@ -111,11 +116,15 @@ const ModalEditNote = (props) => {
         </button>
         <div className={form}>
           <Text text="Edit your note" fontsize="1.8rem" />
-          <form onSubmit={onSubmit}>
+          <form onSubmit={onSubmit} method="PUT" id="editNote">
+            <div className={tick}>
+              <input type="checkbox" id="private" name="private" value="true" onChange={(e) => setNote({ ...note, private: e.target.checked })} />
+              <label htmlFor="private"> Private? </label>
+            </div>
             <div>
               <input
                 text="Title"
-                name="title"
+                name="Title"
                 className={input}
                 placeholder={note.title}
                 onChange={updateField}
@@ -124,6 +133,7 @@ const ModalEditNote = (props) => {
             <div>
               <input
                 title="Text"
+                name="Text"
                 className={input}
                 placeholder={note.text}
                 onChange={updateField}
@@ -132,6 +142,7 @@ const ModalEditNote = (props) => {
             <div>
               <input
                 title="Image"
+                name="Image"
                 className={input}
                 placeholder={note.image}
                 onChange={updateField}
@@ -142,13 +153,12 @@ const ModalEditNote = (props) => {
                 title="Which plant?"
                 values={myPlantsNames}
                 cb={handleSelectChange}
-                value={note.plant}
+                placeholder={note.plant}
+                onChange={updateField}
               />
             </div>
             <div>
-              <button type="submit" className={button}>
-                Save
-              </button>
+              <Button type="submit" text="Save" />
             </div>
           </form>
         </div>
@@ -160,7 +170,7 @@ const ModalEditNote = (props) => {
 ModalEditNote.propTypes = {
   show: PropTypes.bool.isRequired,
   closeModal: PropTypes.func.isRequired,
-  noteId: PropTypes.number.isRequired
+  noteId: PropTypes.string.isRequired
 };
 
 export default ModalEditNote;
