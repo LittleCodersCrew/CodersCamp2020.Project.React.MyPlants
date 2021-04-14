2ab1aebd9440316598dee54dc6fee6f0c1187593
Ada Krupa <adriannakrupa01@gmail.com>
2021-04-14 03:23:39 +0200

    Add responsivity for modal on AddPlant page

--- src/containers/AddPlant/AddPlant.js
+++ src/containers/AddPlant/AddPlant.js
@@ -252,55 +252,55 @@ const AddPlant = () => {
               <div className={styles.details}>
                 <p>
                   <span>Species:</span>
-                  <span>{plant.species}</span>
+                  <span className={styles.italic}>{plant.species}</span>
                 </p>
                 <p>
                   <span>Min temperature:</span>
-                  <span>{plant.min_temperature} °C</span>
+                  <span className={styles.italic}>{plant.min_temperature} °C</span>
                 </p>
                 <p>
                   <span>Max temperature:</span>
-                  <span>{plant.max_temperature} °C</span>
+                  <span className={styles.italic}>{plant.max_temperature} °C</span>
                 </p>
                 <p>
                   <span>Sunlight:</span>
-                  <span>{plant.sunlight}</span>
+                  <span className={styles.italic}>{plant.sunlight}</span>
                 </p>
                 <p>
                   <span>Humidity:</span>
-                  <span>{plant.humidity}</span>
+                  <span className={styles.italic}>{plant.humidity}</span>
                 </p>
                 <p>
                   <span>Watering:</span>
-                  <span>{plant.watering}</span>
+                  <span className={styles.italic}>{plant.watering}</span>
                 </p>
                 <p>
                   <span>Watering method:</span>
-                  <span>{plant.watering_method}</span>
+                  <span className={styles.italic}>{plant.watering_method}</span>
                 </p>
                 <p>
                   <span>Application:</span>
-                  <span>{plant.application}</span>
+                  <span className={styles.italic}>{plant.application}</span>
                 </p>
                 <p>
                   <span>Subsoil: </span>
-                  <span>{plant.subsoil}</span>
+                  <span className={styles.italic}>{plant.subsoil}</span>
                 </p>
                 <p>
                   <span>Conditioners: </span>
-                  <span>{plant.conditioners}</span>
+                  <span className={styles.italic}>{plant.conditioners}</span>
                 </p>
                 <p>
                   <span>Spraying: </span>
-                  <span>{plant.spraying}</span>
+                  <span className={styles.italic}>{plant.spraying}</span>
                 </p>
                 <p>
                   <span>Toxicity: </span>
-                  <span>{plant.toxicity?.human ? 'yes' : 'no'}</span>
+                  <span className={styles.italic}>{plant.toxicity?.human ? 'yes' : 'no'}</span>
                 </p>
                 <p>
                   <span>Safe for domestic animals: </span>
-                  <span>{plant.toxicity?.animal ? 'no' : 'yes'}</span>
+                  <span className={styles.italic}>{plant.toxicity?.animal ? 'no' : 'yes'}</span>
                 </p>
               </div>
             </div>
@@ -310,11 +310,14 @@ const AddPlant = () => {
             <Button text="Delete" onClick={deleteHandler} />
           </div>
           <div className={styles.text}>
-            <Text text="Thank you for your participation!" fontsize="2rem" />
+            <Text
+              text="Thank you for your participation!"
+              fontsize={width > 1024 ? '2em' : '1.5em'}
+            />
             <Text
               text="As soon as it is verified by our team, this plant will be available for others in our
               base."
-              fontsize="2rem"
+              fontsize={width > 1024 ? '2em' : '1.5em'}
             />
           </div>
           <div className={styles.save}>
