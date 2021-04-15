a2c57ade48b1c97dc04c2151b0c3b46934b92180
unknown <nilphym@gmail.com>
2021-04-15 11:25:10 +0200

    Change to hash router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/CodersCamp2020.Project.React.MyPlants">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
